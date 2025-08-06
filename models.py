f7ffcb69f60913b1d6ffd2c35a3c3953c17d958a
don <don@don.don>
2025-08-06 14:20:19 +0200

    added new column for the admin panel

--- models.py
+++ models.py
@@ -46,3 +46,4 @@ class SMSRow(NamedTuple):
     lineID: int
     childFullName: str
     lineCode: str
+    childClassName: str
--- sql/today.sql
+++ sql/today.sql
@@ -1,9 +1,12 @@
 SELECT
     B.*,
-    C.sysTitle,
-    L.KodLinii
+    C.sysTitle AS ImieDziecka,
+    L.KodLinii,
+    K.sysTitle AS Klasa
 FROM
     tblsmsbus AS B
 JOIN r_stoucznia AS C ON B.idDziecka = C.id
 JOIN r_stolinia AS L ON B.idLinii = L.id
-WHERE B.dataDocelowa = CURDATE();
+JOIN r_stoklasa AS K ON C.KlasaDU = K.id
+WHERE B.dataDocelowa = CURDATE()
+ORDER BY L.kodLinii, Klasa, C.Nazwisko, C.Imie;
